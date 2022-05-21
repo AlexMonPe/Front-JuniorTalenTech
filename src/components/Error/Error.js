9d2dd6a078196a50e5e71deb009ef35c1cb66d81
Alejandro <Alex_bcn10@hotmail.es>
2022-05-21 14:58:38 +0200

    create: error component to show error inside components and fix error boundaries

---
+++ src/components/Error/Error.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+export class ErrorComponent extends React.Component {
+  state = {
+    hasError: false,
+    message: "",
+  };
+  state = {
+    Error: {},
+    message: "",
+  };
+
+  componentDidCatch(error) {
+    this.setState({
+      hasError: true,
+      message: error.message,
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error">
+          there was an error
+          {this.state.message}
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
