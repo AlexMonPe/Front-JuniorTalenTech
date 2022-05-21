cccd214e767704a326d1f16f06a690bf3b30e24a
Alejandro <Alex_bcn10@hotmail.es>
2022-05-21 15:16:14 +0200

    add: route of registerCandidate in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { ErrorComponent } from './components/Error/Error';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterCandidate from './containers/RegisterCandidate/RegisterCandidate';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <ErrorComponent>
       <BrowserRouter>
         <Routes>
+        <Route path="/registerCandidate" element={<RegisterCandidate />} ></Route>
         </Routes>
       </BrowserRouter>
     </ErrorComponent>
