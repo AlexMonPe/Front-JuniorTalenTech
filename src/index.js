6760eb1b0fb01fbbf81470a1978e7dea78d8f49c
Alejandro <Alex_bcn10@hotmail.es>
2022-05-23 21:27:41 +0200

    edit: reactdom import to /client in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./scss/main.scss";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
