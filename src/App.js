9a6f74968c610a203f29188d33bfd40f96dd38d9
Alejandro <Alex_bcn10@hotmail.es>
2022-05-23 10:51:13 +0200

    add: error component, header component and bootstrap in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.scss';
-import { ErrorComponent } from './components/Error/Error';
+import { ErrorComponent } from './components/Error/Error.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RegisterCandidate from './containers/RegisterCandidate/RegisterCandidate';
+import RegisterCandidate from './containers/RegisterCandidate/RegisterCandidate.js';
+import Header from './components/Header/Header.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
@@ -9,6 +12,7 @@ function App() {
     <div className="App">
     <ErrorComponent>
       <BrowserRouter>
+      <Header />
         <Routes>
         <Route path="/registerCandidate" element={<RegisterCandidate />} ></Route>
         </Routes>
