e064b92d2e8e2a129f6add7c1a302b589f6da5d4
Alejandro <Alex_bcn10@hotmail.es>
2022-05-24 13:44:56 +0200

    add: login key in apiconsumer

--- src/services/apiConsumer.js
+++ src/services/apiConsumer.js
@@ -13,4 +13,21 @@ export const apiConsumer = {
       console.log(error, "error in register candidate apiconsumer");
     }
   },
+  login: async (loginData) => {
+    try {
+      let loginUser = await fetch(
+        "http://localhost:1919/users/login",
+        {
+          method: "POST",
+          body: JSON.stringify(loginData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return await loginUser.json();
+    } catch (error) {
+      console.log(error, "Error login in user apiconsumer");
+    }
+  },
 };
