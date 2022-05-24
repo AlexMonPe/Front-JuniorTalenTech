f905300b7d0263115fdd6f20ceb8b6ca731b0a39
Alejandro <Alex_bcn10@hotmail.es>
2022-05-24 12:00:02 +0200

    clean: prettier code indented

--- src/services/apiConsumer.js
+++ src/services/apiConsumer.js
@@ -1,20 +1,16 @@
 export const apiConsumer = {
-    registerCandidate: async (formData) =>{
-      try {
-        const createCandidate = await fetch(
-            "http://localhost:1919/candidates/",
-            {
-              method: "POST",
-              body: JSON.stringify(formData),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-          return await createCandidate.json();
+  registerCandidate: async (formData) => {
+    try {
+      const createCandidate = await fetch("http://localhost:1919/candidates/", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return await createCandidate.json();
     } catch (error) {
-        console.log(error, "error in register candidate apiconsumer");
-    }  
+      console.log(error, "error in register candidate apiconsumer");
     }
-    
-}
+  },
+};
