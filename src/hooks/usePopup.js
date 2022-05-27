32161dbdf96f18a6269c588f57402b72a096e714
Alejandro <Alex_bcn10@hotmail.es>
2022-05-27 18:04:15 +0200

    fix: error autoimport

--- src/hooks/usePopup.js
+++ src/hooks/usePopup.js
@@ -1,11 +1,10 @@
 import { useDispatch } from "react-redux";
-import actionCreator from "../store/actionTypes";
-import { SHOW_POPUP } from "../store/typesVar";
+import actionCreator from "../store/actionTypes.js";
+import { SHOW_POPUP } from "../store/typesVar.js";
 
-const usePopup = () => {
+export const usePopup = () => {
     const dispatch = useDispatch()
       return (text) => {
       dispatch(actionCreator(SHOW_POPUP, text))
   };
 };
-export default usePopup;
