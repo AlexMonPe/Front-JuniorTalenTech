4d21e06aa4d4ef31bff463b4204bfc78c1b64e31
Alejandro <Alex_bcn10@hotmail.es>
2022-05-27 17:53:39 +0200

    create: popup component with closepopup function

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import actionCreator from "../../store/actionTypes.js";
+import { CLOSE_POPUP } from "../../store/typesVar.js";
+
+
+export const PopUp = () => {
+  const dispatch = useDispatch();
+  const popupState = useSelector((state) => state.general.popup);
+
+  const closePopUp = () => {
+    dispatch(actionCreator(CLOSE_POPUP));
+  };
+  setTimeout(closePopUp, 3000);
+  return (
+    <div>
+      <div className="popup">
+        <p>{popupState.text}</p>
+      </div>
+    </div>
+  );
+};
