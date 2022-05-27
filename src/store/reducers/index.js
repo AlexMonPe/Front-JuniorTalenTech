06ff681c9b576f48df4ae2ac2d1f8a2928b41461
Alejandro <Alex_bcn10@hotmail.es>
2022-05-27 17:52:35 +0200

    create: general reducer for general states like popup

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { candidateReducer } from "./candidateReducer.js";
+import { generalReducer } from "./generalReducer.js";
 
 
 const reducers = {
+    general: generalReducer,
     candidate: candidateReducer,
 }
 
