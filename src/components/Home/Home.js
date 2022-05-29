bc105629317500cbf59357ef74ba92e91c78a380
Alejandro <Alex_bcn10@hotmail.es>
2022-05-29 22:07:04 +0200

    add: home html

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
-
 export const Home = () => {
-    return (
-        <div className="">
-            <div></div>
-        </div>
-    );
-};
+  return (
+    <div className="home-container">
+      <div className="home">
+        <h2>Bienvenid@ a JuniorTalenTech</h2>
+        <div className="content-home">
+          <p>Para los candidatos:</p>
+          <ul>
+            <li>Si eres Junior con poca experiencia o recién titulado.</li>
+            <li>Si estas cansado de inscribirte en ofertas continuamente.</li>
+            <li>Si estas motivado a aprender y desarrollar tu carrera.</li>
+          </ul>
+          <p>
+            No dudes en registrarte, las empresas te contactaran para conocerte,
+            tan solo tienes que inscribirte.
+          </p>
 
+          <p>Para las empresas:</p>
+          <ul>
+            <li>Buscas talento.</li>
+            <li>Buscas motivación.</li>
+            <li>Mejor portal web para buscar candidatos Tech</li>
+          </ul>
+        </div>
+        <div className="d-flex justify-content-center col-12">
+          <button className="btn btn-outline-info text-white p-3 me-5">Empresas</button>
+          <button className="btn btn-outline-info text-white p-3">Candidatos</button>
+        </div>
+      </div>
+    </div>
+  );
+};
