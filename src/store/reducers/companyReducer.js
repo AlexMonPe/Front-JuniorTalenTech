3c5e48f7450ac56ca59e7b1adef9ec4da2305bb8
Alejandro <Alex_bcn10@hotmail.es>
2022-05-29 14:32:18 +0200

    create: company reducer in redux

---
+++ src/store/reducers/companyReducer.js
@@ -0,0 +1,25 @@
+import { HANDLE_INPUT_COMPANY } from "../typesVar.js";
+
+const initialState = {
+  form: {
+    name: "",
+    title: "",
+    description: "",
+    ubication: "",
+    website: "",
+    cif: "",
+    phone_number: "",
+    employees: "",
+  },
+};
+
+export const companyReducer = (state = initialState, action) => {
+  if (action.type === HANDLE_INPUT_COMPANY) {
+    return {
+      ...state,
+      form: action.payload,
+    };
+  }
+
+  return state;
+};
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import { candidateReducer } from "./candidateReducer.js";
+import { companyReducer } from "./companyReducer.js";
 import { generalReducer } from "./generalReducer.js";
 
 
 const reducers = {
     general: generalReducer,
     candidate: candidateReducer,
+    company: companyReducer,
 }
 
 export default combineReducers(reducers);
