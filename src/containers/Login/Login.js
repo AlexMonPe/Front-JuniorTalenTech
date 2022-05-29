1aa27e40958fa473eb86815dab1ea9ec35eb9b94
Alejandro <Alex_bcn10@hotmail.es>
2022-05-29 22:08:48 +0200

    edit: login indent

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,4 +1,3 @@
-import Input from "../../components/Input/Input.js";
 import { usePopup } from "../../hooks/usePopup.js";
 import { apiConsumer } from "../../services/apiConsumer.js";
 
@@ -13,19 +12,17 @@ const Login = () => {
       };
 
       const loginUser = await apiConsumer.login(loginData);
-      
-      localStorage.setItem('token',loginUser.token)
-      localStorage.setItem('id',loginUser.id)
-      localStorage.setItem('role',loginUser.role)
 
-      if(loginUser.error){
-        popUp(`${loginUser.error}`)
-      }else{
-        console.log(loginUser)
-        popUp(`Bienvenid@ a JuniorTalenTech`)
+      localStorage.setItem("token", loginUser.token);
+      localStorage.setItem("id", loginUser.id);
+      localStorage.setItem("role", loginUser.role);
+
+      if (loginUser.error) {
+        popUp(`${loginUser.error}`);
+      } else {
+        console.log(loginUser);
+        popUp(`Bienvenid@ a JuniorTalenTech`);
       }
-     
-
     } catch (error) {
       console.log(error);
     }
@@ -33,40 +30,40 @@ const Login = () => {
   return (
     <div className="login-page">
       <form onSubmit={loginSubmit} className="login">
-      <div className="container-login-data ">
-        <h2 className="mb-5 text-center">LOGIN</h2>
-        <div className="form-floating mb-4 col-12">
-          <Input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            placeholder="example@example.com"
-          />
-          <label htmlFor="floatingInput">Email</label>
-        </div>
-        <div className="form-floating mb-4 col-12">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            placeholder="contraseña"
-            minLength={6}
-            required
-          />
-          <label htmlFor="floatingInput">Contraseña</label>
-        </div>
-        <div className="text-center">
-          <Input
-            className="btn btn-outline-info mb-4 text-dark col-8 p-2"
-            type="submit"
-            value="Sign in"
-          />
-        </div>
-        <p className="loginRedirect">
-          New in JuniorTalenTech? <a href="/home">Sign up now</a>
-        </p>
+        <div className="container-login-data ">
+          <h2 className="mb-5 text-center">INICIAR SESION</h2>
+          <div className="form-floating mb-4 col-12">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="example@example.com"
+            />
+            <label htmlFor="floatingInput">Email</label>
+          </div>
+          <div className="form-floating mb-4 col-12">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="contraseña"
+              minLength={6}
+              required
+            />
+            <label htmlFor="floatingInput">Contraseña</label>
+          </div>
+          <div className="text-center">
+            <input
+              className="btn btn-outline-info mb-4 text-dark col-8 p-2"
+              type="submit"
+              value="Acceder"
+            />
+          </div>
+          <span className="loginRedirect">
+            Nuev@ en JuniorTalenTech? <a href="/home">Regístrate ahora</a>
+          </span>
         </div>
       </form>
     </div>
