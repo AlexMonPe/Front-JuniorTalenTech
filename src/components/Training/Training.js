ccfa854a9e0d1f48a78a6c071faa82c087babfa2
Alejandro <Alex_bcn10@hotmail.es>
2022-05-31 14:00:24 +0200

    currprofile map if is editable

--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -5,12 +5,17 @@ export const Training = ({
   handleTrainingChange,
   removeTraining,
   addTraining,
+  currProfile
 }) => {
   const training = useSelector((state) => state.candidate.training);
+  const isEditable = useSelector((state) => state.general.isEditable);
+
+
+
 
   return (
     <Fragment>
-      {training.map((training, index) => {
+      {isEditable ? currProfile.map((training, index) => {
         return (
           <div className="container-form-data mb-5" key={index}>
             <h2 className="col-12 mb-4 text-center">Formación</h2>
@@ -20,7 +25,7 @@ export const Training = ({
                 id="floatingSelect"
                 aria-label="Floating label select example"
                 name="level"
-                value={training.level}
+                defaultValue={isEditable ? currProfile[index].level : training.level}
                 onChange={(event) => handleTrainingChange(index, event)}
                 required
               >
@@ -49,7 +54,7 @@ export const Training = ({
                 className="form-control"
                 id="specialty"
                 name="specialty"
-                value={training.specialty}
+                defaultValue={isEditable ? currProfile[index].specialty : training.specialty}
                 placeholder="Especialidad"
                 onChange={(event) => handleTrainingChange(index, event)}
                 required
@@ -63,7 +68,7 @@ export const Training = ({
                   placeholder="Nombre centro"
                   id="center"
                   name="center"
-                  value={training.center}
+                  defaultValue={isEditable ? currProfile[index].center : training.center}
                   onChange={(event) => handleTrainingChange(index, event)}
                   required
                 />
@@ -77,7 +82,7 @@ export const Training = ({
                 id="start_year"
                 placeholder="año"
                 name="start_year"
-                value={training.start_year}
+                defaultValue={isEditable ? currProfile[index].start_year : training.start_year}
                 pattern="^[0-9]+$"
                 minLength={4}
                 maxLength={4}
@@ -93,7 +98,7 @@ export const Training = ({
                 id="finish_year"
                 placeholder="año"
                 name="finish_year"
-                value={training.finish_year}
+                defaultValue={isEditable ? currProfile[index].finish_year : training.finish_year}
                 pattern="^[0-9]+$"
                 minLength={4}
                 maxLength={4}
@@ -117,7 +122,115 @@ export const Training = ({
             </div>
           </div>
         );
-      })}
+      })
+    : training.map((training, index) => {
+      return (
+        <div className="container-form-data mb-5" key={index}>
+          <h2 className="col-12 mb-4 text-center">Formación</h2>
+          <div className="form-floating mb-4 col-12 col-sm-5">
+            <select
+              className="form-select"
+              id="floatingSelect"
+              aria-label="Floating label select example"
+              name="level"
+              defaultValue={isEditable ? currProfile[index].level : training.level}
+              onChange={(event) => handleTrainingChange(index, event)}
+              required
+            >
+              <option value="">Selecciona nivel</option>
+              <option value="Educacion Secundaria Obligatoria">
+                Educacion Secundaria Obligatoria
+              </option>
+              <option value="Bachillerato">Bachillerato</option>
+              <option value="Ciclo Formativo de grado Medio">
+                Ciclo Formativo de grado Medio
+              </option>
+              <option value="Ciclo Formativo de grado Superior">
+                Ciclo Formativo de grado Superior
+              </option>
+              <option value="Licenciatura">Licenciatura</option>
+              <option value="Diplomatura">Diplomatura</option>
+              <option value="Ingenieria">Ingenieria</option>
+              <option value="Master">Master</option>
+              <option value="Otras titulaciones">Otras titulaciones</option>
+            </select>
+            <label htmlFor="floatingSelect">Nivel de estudios</label>
+          </div>
+          <div className="form-floating mb-4 col-12 col-sm-5">
+            <input
+              type="text"
+              className="form-control"
+              id="specialty"
+              name="specialty"
+              defaultValue={isEditable ? currProfile[index].specialty : training.specialty}
+              placeholder="Especialidad"
+              onChange={(event) => handleTrainingChange(index, event)}
+              required
+            />
+            <label htmlFor="floatingInput">Especialidad</label>
+          </div>
+          <div className="form-floating mb-4 col-12 col-sm-11">
+            <div className="form-floating">
+              <input
+                className="form-control"
+                placeholder="Nombre centro"
+                id="center"
+                name="center"
+                defaultValue={isEditable ? currProfile[index].center : training.center}
+                onChange={(event) => handleTrainingChange(index, event)}
+                required
+              />
+              <label htmlFor="floatingTextarea">Centro / Universidad</label>
+            </div>
+          </div>
+          <div className="form-floating mb-4 col-12 col-sm-11 col-md-5 col-lg-5">
+            <input
+              type="text"
+              className="form-control"
+              id="start_year"
+              placeholder="año"
+              name="start_year"
+              defaultValue={isEditable ? currProfile[index].start_year : training.start_year}
+              pattern="^[0-9]+$"
+              minLength={4}
+              maxLength={4}
+              onChange={(event) => handleTrainingChange(index, event)}
+              required
+            />
+            <label htmlFor="floatingInput">Año inicio ( Ej: 2008 )</label>
+          </div>
+          <div className="form-floating mb-4 col-12 col-sm-11 col-md-5 col-lg-5">
+            <input
+              type="text"
+              className="form-control"
+              id="finish_year"
+              placeholder="año"
+              name="finish_year"
+              defaultValue={isEditable ? currProfile[index].finish_year : training.finish_year}
+              pattern="^[0-9]+$"
+              minLength={4}
+              maxLength={4}
+              onChange={(event) => handleTrainingChange(index, event)}
+              required
+            />
+            <label htmlFor="floatingInput startDate">
+              Año fin ( Ej: 2021 )
+            </label>
+          </div>
+          <div>
+            <button className="btn btn-secondary m-2" onClick={addTraining}>
+              Añadir
+            </button>
+            <button
+              className="btn btn-secondary m-2"
+              onClick={(event) => removeTraining(index, event)}
+            >
+              Eliminar
+            </button>
+          </div>
+        </div>
+      );
+    })}
     </Fragment>
   );
 };
